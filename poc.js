950b441fce7c3362a977084f50e345e75508dab0
Andoryuuta <andrew.gutekanst@gmail.com>
2017-03-11 02:05:20 -0700

    Update poc.js

--- poc.js
+++ poc.js
@@ -169,8 +169,8 @@ for (var i = 0; i < 0x1000; i++){
 				
                        		
 				//alert("Pushed stale");
-				var original_val = bufs[i][k];
-				var original_val1 = bufs[i][k+1];
+				//var original_val = bufs[i][k];
+				//var original_val1 = bufs[i][k+1];
 				
 				bufs.push(stale);
 				stale[0] = fc;
