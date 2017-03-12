f334130455cec1b871e3cb852c05b8a1d176913d
Andoryuuta <andrew.gutekanst@gmail.com>
2017-03-12 00:26:35 -0700

    Update poc.js

--- poc.js
+++ poc.js
@@ -37,6 +37,7 @@ function sleep(milliseconds) {
 	var start = new Date().getTime();
 	for(var i = 0; i < 1e7; i++) {
 		if((new Date().getTime() - start) > milliseconds) {
+			alert(i);
 			break;
 		}
 	}
