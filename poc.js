29e5895590fa1d52cf5c227916a9a146f286a16a
Andoryuuta <andrew.gutekanst@gmail.com>
2017-03-12 00:28:51 -0700

    Sleep makes everything better.

--- poc.js
+++ poc.js
@@ -37,7 +37,6 @@ function sleep(milliseconds) {
 	var start = new Date().getTime();
 	for(var i = 0; i < 1e7; i++) {
 		if((new Date().getTime() - start) > milliseconds) {
-			alert(i);
 			break;
 		}
 	}
